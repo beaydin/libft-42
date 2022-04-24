3c37b08461e4273b8968ab86591840f79c8399d5
Beyza Aydın <100858705+beaydin@users.noreply.github.com>
2022-04-25 02:01:38 +0300

    24.0424.0424.0424.04

--- libft.h
+++ libft.h
@@ -33,5 +33,6 @@ char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
+char *ft_strmapi(char const *s, char (*f)(unsignedint, char));
 
 #endif
